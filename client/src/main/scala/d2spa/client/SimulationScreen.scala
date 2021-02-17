a2f472e1b11d76c31d444573951ebaf2113a6362
domschoen <dschoenenberger@mac.com>
2021-02-17 23:50:50 +0100

    Test 5 xyz, failing

--- client/src/main/scala/d2spa/client/SimulationScreen.scala
+++ client/src/main/scala/d2spa/client/SimulationScreen.scala
@@ -112,7 +112,7 @@ object SimulationScreen {
 
 
   private val component = ScalaComponent.builder[Props]("SimulationScreen")
-    .initialState(State(Some("abc"),Some("abd"),Some("kji")))
+    .initialState(State(Some("abc"),Some("abd"),Some("xyz")))
     .renderBackend[Backend]
     .build
 
