bb23af0af60624e39c7a4933d848c51fd1c05443
domschoen <dschoenenberger@mac.com>
2021-02-18 15:12:09 +0100

    prepare test 6: abcm -> abcn, rijk

--- client/src/main/scala/d2spa/client/SimulationScreen.scala
+++ client/src/main/scala/d2spa/client/SimulationScreen.scala
@@ -112,7 +112,7 @@ object SimulationScreen {
 
 
   private val component = ScalaComponent.builder[Props]("SimulationScreen")
-    .initialState(State(Some("abc"),Some("abd"),Some("xyz")))
+    .initialState(State(Some("abcm"),Some("abcn"),Some("rijk")))
     .renderBackend[Backend]
     .build
 
